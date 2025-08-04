2d62faf19d5754bb47ac1053eeb3f8a1d2140913
ymk <mkyms@outlook.com>
2025-08-04 16:20:52 +0800

    增加友链

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import { Box, Container, Flex, Link, Section } from "@radix-ui/themes";
+import { Box, Container, Flex, Heading, Link, Section } from "@radix-ui/themes";
 import { useTranslations } from "next-intl";
 
 export default function Footer() {
@@ -7,22 +7,42 @@ export default function Footer() {
   return (
     <footer className="w-full border-t backdrop-blur-sm bg-background/95 ">
       <Flex justify={"between"} align={"center"} direction={"column"} gap={"2"} p="2">
-        <Flex justify={"center"} gap={"4"}>
+        <Flex justify={"center"} gap={"8"} className="w-full">
 
-          <Link
-            href="/editor"
-            className="text-sm text-muted-foreground hover:text-primary"
-          >
-            Editor
-          </Link>
+          <Flex gap={"2"} direction={"column"}>
+            <Heading as="h2" size="4">Tools</Heading>
+            <Link
+              href="/editor"
+              className="text-sm text-muted-foreground hover:text-primary"
+            >
+              Editor
+            </Link>
+          </Flex>
 
-          <Link
-            href="/do-not-write-on-this-page"
-            className="text-sm text-muted-foreground hover:text-primary"
-          >
-            Do Not Write On This Page
-          </Link>
+          <Flex gap={"2"} direction={"column"}>
+            <Heading as="h2" size="4">Resources</Heading>
+            <Link
+              href="/do-not-write-on-this-page"
+              className="text-sm text-muted-foreground hover:text-primary"
+            >
+              Do Not Write On This Page
+            </Link>
+          </Flex>
+
+          <Flex gap={"2"} direction={"column"}>
+            <Heading as="h2" size="4">Other Links</Heading>
+
+            <Link
+              href="https://uiuxdeck.com/"
+              className="text-sm text-muted-foreground hover:text-primary"
+              target="_blank"
+            >
+              UIUXDECK
+            </Link>
+          </Flex>
         </Flex>
+
+
         <Box className="text-sm text-muted-foreground">
           {f("copyright", { year: new Date().getFullYear() })}
         </Box>
