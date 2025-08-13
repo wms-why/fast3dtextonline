a0c9ae3bfcaed394ca2e246cac0a76ebc3d5325e
ymk <mkyms@outlook.com>
2025-08-13 10:18:36 +0800

    增加seo相关配置

--- src/app/[locale]/features-form/page.tsx
+++ src/app/[locale]/features-form/page.tsx
@@ -1,6 +1,7 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import { Box, Flex } from "@radix-ui/themes";
+import { Metadata } from "next";
 
 export default function Page() {
 
@@ -15,4 +16,51 @@ export default function Page() {
     </Flex>
 
   )
+}
+
+const host = process.env.NEXT_PUBLIC_HOST;
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ locale: string }>;
+}): Promise<Metadata> {
+  const { locale } = await params;
+
+  const name = "features-form";
+
+  const title = "new features wanted";
+  const description = title;
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      url: `${host}/${locale}/${name}`,
+      images: [
+        {
+          url: `${process.env.NEXT_PUBLIC_HOST}/og-image.png`,
+          width: 1200,
+          height: 630,
+          alt: title,
+        },
+      ],
+      locale: locale,
+      type: "website",
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [`${process.env.NEXT_PUBLIC_HOST}/og-image.png`],
+    },
+    alternates: {
+      canonical: `${host}/${name}`,
+      languages: {
+        en: `${host}/en/${name}`,
+        zh: `${host}/zh/${name}`,
+      },
+    },
+  };
 }
