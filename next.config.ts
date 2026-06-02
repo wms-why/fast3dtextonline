3c62fab38deaa10d09b10affc17240df146264cb
wms-why <mkyms@outlook.com>
2026-06-02 15:45:57 +0800

    chore(next): disable next/image optimization for Cloudflare Workers

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  images: { unoptimized: true },
+};
 
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
